021147d11c1971b74e2370c3894eff25d00173e1
fabianhabil <fabianhabilramdhan@gmail.com>
2024-02-24 23:27:16 +0700

    feat: two entity injected example

--- src/module/healthcheck/healthcheck.module.ts
+++ src/module/healthcheck/healthcheck.module.ts
@@ -3,9 +3,10 @@ import { HealthcheckController } from './healthcheck.controller';
 import { LoggerModule } from 'src/module/logger/logger.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Health } from 'src/database/entities/health.entity';
+import { Test } from 'src/database/entities/test.entity';
 
 @Module({
-  imports: [LoggerModule, TypeOrmModule.forFeature([Health])],
+  imports: [LoggerModule, TypeOrmModule.forFeature([Health, Test])],
   controllers: [HealthcheckController],
 })
 export class HealthcheckModule {}
