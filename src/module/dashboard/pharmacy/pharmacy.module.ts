d92b665940d6d33351b446dca7ef8a90b3f837ba
Jesslyn Audrey <jesslynaudreyy@gmail.com>
2024-11-12 19:17:49 +0700

    pharmacy queue

---
+++ src/module/dashboard/pharmacy/pharmacy.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PharmacyQueue } from 'src/database/entities/pharmacyqueue.entity';
+import { LoggerModule } from 'src/module/logger/logger.module';
+import { PharmacyController } from './pharmacy.controller';
+import { PharmacyService } from './pharmacy.service';
+import { Appointment } from 'src/database/entities/appointment.entitity';
+import { CashierQueue } from 'src/database/entities/cashierqueue.entity';
+
+@Module({
+  imports: [LoggerModule, TypeOrmModule.forFeature([PharmacyQueue, CashierQueue, Appointment])],
+  controllers: [PharmacyController],
+  providers: [PharmacyService],
+})
+export class PharmacyModule {}
